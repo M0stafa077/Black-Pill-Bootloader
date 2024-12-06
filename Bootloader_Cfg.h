8e6944f9b5e95ed5ebf59d859d5d6fa354a6084a
Mostafa Asaad <ma4190243@gmail.com>
2024-12-06 14:21:44 +0200

    Bootloader Configuration file

---
+++ Bootloader_Cfg.h
@@ -0,0 +1,13 @@
+/*
+ * Bootloader_Cfg.h
+ *
+ *  Created on: Aug 20, 2024
+ *      Author: mostafa
+ */
+
+#ifndef INC_BOOTLOADER_BOOTLOADER_CFG_H_
+#define INC_BOOTLOADER_BOOTLOADER_CFG_H_
+
+#define USER_APPLICATION_SECTOR			FLASH_SECTOR_2
+
+#endif /* INC_BOOTLOADER_BOOTLOADER_CFG_H_ */
